57134430fa25e56bd07ed4c2cd2709b4ca3c7b37
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-21 14:38:20 +0800

    - initialized project

---
+++ Flash-Cards/Flash_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flash_CardsApp.swift
+//  Flash-Cards
+//
+//  Created by Ronjie Diafante Man-on on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Flash_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Flash-Cards/View/Main View/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flash-Cards
+//
+//  Created by Ronjie Diafante Man-on on 5/21/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
